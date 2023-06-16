ee4adeb089590ec6aa57c196cf7e915869842619
Omarbelh <belhadjomar33@gmail.com>
2023-06-16 11:04:55 -0700

    why

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+* this code for generate alphabet in low case
+*/
+int main()
+{
+
+	// Get the character to be written
+	char ch = 'a';
+
+	// Write the Character to stdout
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	return (0);
+}
