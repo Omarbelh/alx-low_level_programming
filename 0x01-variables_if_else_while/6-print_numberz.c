15a587d69abb3099811296fed623321ee4f52f36
Omarbelh <belhadjomar33@gmail.com>
2023-06-16 14:43:54 -0700

    r

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main. putchar numberz from 0 to 9,
+ * followed by new line,
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for ( n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
