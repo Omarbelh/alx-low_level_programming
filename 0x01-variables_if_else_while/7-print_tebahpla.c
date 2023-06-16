21934b29f0fe9b7bab124440bfc92189f44f19bd
Omarbelh <belhadjomar33@gmail.com>
2023-06-16 15:47:29 -0700

    r

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
+
 /**
- * main - prints all the numbers of base 16 in lowercase
- * followed by new line,
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char lowcase;
+	int n;
+	char ch;
 
-    for (lowcase = '0';lowcase <= '9'; lowcase++)
-    {
-        putchar(lowcase);
-    }
-
-    lowcase = 'a';
-
-    for (lowcase = 'a';lowcase <= 'f'; lowcase++)
-    {
-        putchar(lowcase);
-    }
-
-    putchar('\n');
-
-    return 0;
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
