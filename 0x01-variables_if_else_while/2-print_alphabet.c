d0e24282e1cd95169aa2a6ececa2802ad91518a4
Omarbelh <belhadjomar33@gmail.com>
2023-06-16 11:15:24 -0700

    tyt

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 /*
 * this code for generate alphabet in low case
+* Return: Always 0.
 */
 int main(void)
 {
 	char ch = 'a';
-	
+
 	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
+	putchar('\n');
+	
 	return (0);
 }
