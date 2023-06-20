71dd67f065ce37ce37ef926afe8be45c49f80a21
Omarbelh <belhadjomar33@gmail.com>
2023-06-20 15:44:33 -0700

    l

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,11 @@
 /**
  * main - prints the alphabet, in lowercase
  * followed by a new line.
- * Return: Always 0 (succes)
  */
 void print_alphabet(void)
 	
 {
 	for (int a=97; a<=122; a++)
-	{
 		_putchar(a);
-	}
 	_putchar('\n');
-	return (0);
 }
